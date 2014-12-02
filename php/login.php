503339ae02f83174ce32fc77448d81735308fb84
lightning10 <qw_guy@yahoo.com.tw>
2014-12-03 00:58:35 +0800

    登入登出
    可以登入也可以登出囉~

--- php/login.php
+++ php/login.php
@@ -21,13 +21,13 @@
 				
 				//check the correct password
 				if($check_password == $password)	
-					echo "歡迎回來 $row[1]";	
+					echo "~~歡迎回來 $row[1]~~";	
 				else
-					echo "請輸入正確的密碼";
+					echo "請輸入正確的密碼！！";
 			}
 			
 			else
-				echo "您輸入了不存在的帳號";
+				echo "您輸入了不存在的帳號！！";
 		}
 	
 		else
@@ -35,6 +35,6 @@
 	}
 	
 	else
-		echo "請完整輸入帳號和密碼";
+		echo "請完整輸入帳號和密碼！！";
 	
 ?>
